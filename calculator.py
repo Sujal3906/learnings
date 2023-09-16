9290372466d6c03fedb3b773941bbf4693f1999a
Sujal3906 <129257474+Sujal3906@users.noreply.github.com>
2023-09-16 09:21:17 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,17 @@
+print("This is the calculator program in Python:")
+print("\n\n************CALCULATOR*************\n\n")
+operator = int(input("Enter 1 for division\nEnter 2 for multiplication\nEnter 3 for subtraction\nEnter 4 for addition: "))
+a = float(input("Enter number 1: "))
+b = float(input("Enter number 2: "))
+
+if operator == 1:
+    print("Division is:", a / b)
+elif operator == 2:
+    print("Multiplication is:", a * b)
+elif operator == 3:
+    print("Subtraction is:", a - b)
+elif operator == 4:
+    print("Sum is:", a + b)
+else:
+    print("Invalid choice!")
 
